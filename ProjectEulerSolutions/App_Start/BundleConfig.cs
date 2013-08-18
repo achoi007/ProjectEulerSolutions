97dc26b0fd241da014cf58d0b30ec96f0bd53152
achoi <achoi@live.com>
2013-08-18 01:45:56 -0500

    Removed all style sheets, JS libraries.  Not needed for this simple project.

--- ProjectEulerSolutions/App_Start/BundleConfig.cs
+++ ProjectEulerSolutions/App_Start/BundleConfig.cs
@@ -8,46 +8,6 @@ namespace ProjectEulerSolutions
         // For more information on Bundling, visit http://go.microsoft.com/fwlink/?LinkId=254725
         public static void RegisterBundles(BundleCollection bundles)
         {
-            bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
-                        "~/Scripts/jquery-{version}.js"));
-
-            // JQuery UI
-
-            bundles.Add(new ScriptBundle("~/bundles/jqueryui").Include(
-                        "~/Scripts/jquery-ui-{version}.custom.js"));
-
-            bundles.Add(new StyleBundle("~/Content/themes/ui-lightness").Include(
-                "~/Content/themes/ui-lightness/jquery-ui-{version}.custom.css"
-                ));
-
-            // JQuery Mobile
-
-            bundles.Add(new ScriptBundle("~/bundles/jquerymobile").Include(
-                "~/Scripts/jquery.mobile-{version}.js"
-                ));
-
-            bundles.Add(new StyleBundle("~/Content/themes/mobile").Include(
-                "~/Content/themes/mobile/jquery.mobile-{version}.css"
-                ));
-
-            // JQuery validation
-
-            bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
-                        "~/Scripts/jquery.unobtrusive*",
-                        "~/Scripts/jquery.validate*"));
-
-            // JSRender
-
-            bundles.Add(new ScriptBundle("~/bundles/jsrender").Include(
-                "~/Scripts/jsrender-{version}.js"
-                ));
-
-            // Use the development version of Modernizr to develop with and learn from. Then, when you're
-            // ready for production, use the build tool at http://modernizr.com to pick only the tests you need.
-            bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
-                        "~/Scripts/modernizr-*"));
-
-            bundles.Add(new StyleBundle("~/Content/css").Include("~/Content/site.css"));
         }
     }
 }
